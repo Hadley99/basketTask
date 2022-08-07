823c6ed0558d4c80c48195f221c2dfc1c5332d4e
Hadley99 <hadleyfernandes99@gmail.com>
2022-08-07 18:33:17 +0530

    before firebase

--- src/components/products/SingleProduct.jsx
+++ src/components/products/SingleProduct.jsx
@@ -8,16 +8,16 @@ const SingleProduct = ({ product, addToCart }) => {
   return (
     <>
       <div className="font-medium text-xl flex justify-around items-center my-4">
-        <p>{product.name}</p>
+        <p>{product?.name}</p>
         <div className="ml-auto flex justify-between items-center space-x-4">
           <p>
-            <span className="text-gray-400 ">£</span> {product.price}
+            <span className="text-gray-400 ">£</span> {product?.price}
           </p>
 
           <Button
-            data-testid="add-to-cart-button"
+            data-testid="add-button"
             value="Add"
-            disabled={cart.find((p) => p.id === product.id)}
+            disabled={cart.find((p) => p.id === product?.id)}
             onClick={() => addToCart(product)}
           />
         </div>
---
+++ src/components/products/SingleProducts.test.js
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import SingleProduct from "./SingleProduct";
+import { Provider } from "react-redux";
+import { store } from "../../store";
+import SingleBasketItem from "../basket/SingleBasketItem";
+
+describe("SingleProduct", () => {
+  it("testing of add to cart button", async () => {
+    const { getByDisplayValue } = render(
+      <Provider store={store}>
+        <SingleProduct />
+      </Provider>
+    );
+
+    const addButton = screen.getByDisplayValue("Add");
+
+    expect(addButton).toBeInTheDocument();
+  });
+});
