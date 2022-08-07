2006bbd0a53c74ef16d280117360701ae6eeb779
Hadley99 <hadleyfernandes99@gmail.com>
2022-08-07 18:59:38 +0530

    env added

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMCllas4ub4DXgbvUTH1cmgwMhuPdNmMk",
-  authDomain: "product-task-10303.firebaseapp.com",
-  projectId: "product-task-10303",
-  storageBucket: "product-task-10303.appspot.com",
-  messagingSenderId: "585824976999",
-  appId: "1:585824976999:web:ef12d17182500f4bafd4c2",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
